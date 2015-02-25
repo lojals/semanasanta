fa3b98a738992d9d8b547755abc64b67fc019ea3
Jorge R Ovalle Z <jroz9105@gmail.com>
2015-02-25 16:23:19 -0500

    XCode Project

---
+++ Semana Santa/Semana Santa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Semana Santa
+//
+//  Created by Jorge Raul Ovalle Zuleta on 2/25/15.
+//  Copyright (c) 2015 Jorge Ovalle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
